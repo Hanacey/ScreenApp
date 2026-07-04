dd9999daa7955881e8512b50e2cce9e35178a433
Hanacey <hana_chee@s2025.ssts.edu.sg>
2026-07-04 10:04:40 +0800

    Initial Commit

---
+++ ScreenApp/ScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAppApp.swift
+//  ScreenApp
+//
+//  Created by Hana Chee on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
